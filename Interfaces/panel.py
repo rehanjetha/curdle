b41e383d9145bce4eeb7452d5bb806a42864635e
LightBlade12 <rehanjetha@gmail.com>
2023-12-15 14:02:50 -0500

    changes to customtkinter

--- Interfaces/panel.py
+++ Interfaces/panel.py
@@ -1,43 +1,36 @@
 import json
-import sys
-import tkinter
 from tkinter import messagebox, ttk
+import customtkinter as ctk
 
-class Panel(tkinter.Tk):
-    """Curdle Configuration Panel made with Tkinter"""
+class Panel(ctk.CTk):
+    """Curdle Configuration Panel made with ctk"""
 
     def __init__(self):
         """Initialize Panel Class"""
-        super().__init__()  # inherit tkinter root & its methods
+        super().__init__()  # inherit ctk root & its methods
 
         # Class Variables
         self.__bg = "#343541"  # panel background colour
         self.__fg = "#FFFFFF"  # text foreground colour
         self.__size = "600x300"  # panel dimensions
 
+
         # Window Pre-Configuration
         self.title("Cu-rdle Configuration Panel")  # set panel name
         self.geometry(f"{self.__size}")  # set panel size
-        self.resizable(False, False)  # prevent window resizing
         self.configure(bg=self.__bg)  # set background colour
-        self.protocol("WM_DELETE_WINDOW", self.close)  # close entire pgm when closed
-
         self.create_widgets()  # create & place all widgets
 
 
     def create_widgets(self):
         """Method to create & place all widgets"""
 
-        # Use TTK Style Widgets
-        style = ttk.Style(self)  # use TTK styles on root
-        style.configure("TButton", background=self.__bg)  # set bg for buttons
-        style.configure("TLabel", foreground=self.__fg, background=self.__bg)  # set bg & fg for buttons
-
-        # Tkinter Variables
-        self.__diff_var = tkinter.IntVar()  # difficulty variable
-        self.__ai_var = tkinter.BooleanVar()  # ai boolean variable
-        self.__wrd_var = tkinter.IntVar()  # word length variable
-        self.__guess_var = tkinter.IntVar()  # guess length variable
+        # ctk Variables
+        self.__diff_var = ctk.IntVar()  # difficulty variable
+        self.__ai_var = ctk.StringVar(value="on")  # ai boolean variable
+        self.__wrd_var = ctk.IntVar()  # word length variable
+        self.__guess_var = ctk.IntVar()  # guess length variable
+        self.__how_var = ctk.StringVar(value="on")  # tutorial variable
 
         # Default Word Length
         self.__wrd_var.set(5)
@@ -46,34 +39,34 @@ class Panel(tkinter.Tk):
         self.__guess_var.set(6)
 
         # Panel Labels
-        intro_label = ttk.Label(self, text="Welcome to CU-RDLE", font=("Open Sans", 18))  # title header
-        diff_label = ttk.Label(self, text="Difficulty: ")  # difficulty label
-        wrd_len_label = ttk.Label(self, text="Word Length: ")  # word length label
-        guess_label = ttk.Label(self, text="Guess Amount: ")  # guesses label
+        intro_label = ctk.CTkLabel(self, text="Welcome to CU-RDLE", font=("Open Sans", 18))  # title header
+        diff_label = ctk.CTkLabel(self, text="Difficulty: ")  # difficulty label
+        wrd_len_label = ctk.CTkLabel(self, text="Word Length: ")  # word length label
+        guess_label = ctk.CTkLabel(self, text="Guess Amount: ")  # guesses label
 
         # Panel Buttons
-        norm_radio = ttk.Radiobutton(self, text="Normal", variable=self.__diff_var, value=1)  # normal difficulty
-        hard_radio = ttk.Radiobutton(self, text="Hard", variable=self.__diff_var, value=2)  # hard difficulty
-        insane_radio = ttk.Radiobutton(self, text="Insane", variable=self.__diff_var, value=3)  # insane difficulty
-        ai_check = ttk.Checkbutton(self, text="AI Mode", variable=self.__ai_var)  # ai button
-        self.__wrd_len_spin = ttk.Spinbox(self, from_=3, to=6, textvariable=self.__wrd_var)  # word length spinbox
-        self.__guess_spin = ttk.Spinbox(self, from_=3, to=10, textvariable=self.__guess_var)  # guess amt spinbox
-        start_button = ttk.Button(self, text="Start", command=self.start)  # start
-        how_button = ttk.Button(self, text="Tutorial Video", command=self.video)  # tutorial
+        norm_radio = ctk.CTkRadioButton(self, text="Normal", variable=self.__diff_var, value=1)  # normal difficulty
+        hard_radio = ctk.CTkRadioButton(self, text="Hard", variable=self.__diff_var, value=2)  # hard difficulty
+        insane_radio = ctk.CTkRadioButton(self, text="Insane", variable=self.__diff_var, value=3)  # insane difficulty
+        ai_check = ctk.CTkSwitch(self, text="AI Mode", variable=self.__ai_var, onvalue="on", offvalue="off")  # ai button
+        word_len_spin = ttk.Spinbox(self, from_=3, to=6, textvariable=self.__wrd_var)  # word length spinbox
+        guess_spin = ttk.Spinbox(self, from_=3, to=10, textvariable=self.__guess_var)  # guess amt spinbox
+        start_button = ctk.CTkButton(self, text="Start", command=self.start)  # start
+        how_button = ctk.CTkSwitch(self, text="Tutorial Video", variable=self.__how_var, onvalue="on", offvalue="off")  # tutorial
 
         # Panel Layout
-        intro_label.grid(row=0, column=2, pady=(0, 20))  # place intro header
+        intro_label.grid(row=0, column=2, pady=(0, 50))  # place intro header
 
         diff_label.grid(row=2, column=0, sticky="w")
-        norm_radio.grid(row=2, column=2, sticky="w")
-        hard_radio.grid(row=2, column=3, sticky="w")
-        insane_radio.grid(row=2, column=4, sticky="w")
+        norm_radio.grid(row=2, column=1, sticky="w")
+        hard_radio.grid(row=2, column=2, sticky="w")
+        insane_radio.grid(row=2, column=3, sticky="w")
 
         wrd_len_label.grid(row=3, column=0, sticky="w")
-        self.__wrd_len_spin.grid(row=3, column=2)
+        word_len_spin.grid(row=3, column=2)
 
         guess_label.grid(row=4, column=0, sticky="w")
-        self.__guess_spin.grid(row=4, column=2)
+        guess_spin.grid(row=4, column=2)
 
         start_button.grid(row=5, column=4)
         ai_check.grid(row=6, column=0)
@@ -90,35 +83,42 @@ class Panel(tkinter.Tk):
         elif (self.__diff_var.get() == 3):
             diff = "Insane"
         else:
-            messagebox.showerror('Difficulty Error', 'Error: Please choose a difficulty')
-            return
+            messagebox.showerror('Difficulty Error', 'Error: Please choose a difficulty')  # error
+            return  # do not start
 
         # Ensure Valid Values
         try:
             int(self.__wrd_var.get())
         except:
-            messagebox.showerror('Word Length Error', 'Error: Please choose a valid word length')
-            return
+            messagebox.showerror('Word Length Error', 'Error: Please choose a valid word length')  # error
+            return  # do not start
         try:
             int(self.__guess_var.get())
         except:
-            messagebox.showerror('Guess Length Error', 'Error: Please choose a valid guess length')
-            return
-        
+            messagebox.showerror('Guess Length Error', 'Error: Please choose a valid guess length')  # error
+            return  # do not start
+
         # Check Values' Domain
         if (3 <= self.__wrd_var.get() <= 6):
             word_len = self.__wrd_var.get()
         else:
             messagebox.showinfo('Invalid Word Length', 'Defaulting to Word Length of 5')
             word_len = 5
+
         if (3 <= self.__guess_var.get() <= 10):
             guess_len = self.__guess_var.get()
         else:
             messagebox.showinfo('Invalid Guess Length', 'Defaulting to Guess Length of 6')
             guess_len = 6
 
+        # Show/Don't Show Tutorial
+        if (self.__how_var.get() == "on"):
+            show_tuto = True
+        else:
+            show_tuto = False
+
         # Use/Not Use AI
-        if (self.__ai_var.get() == True):
+        if (self.__ai_var.get() == "on"):
             use_ai = True
         else:
             use_ai = False
@@ -128,23 +128,16 @@ class Panel(tkinter.Tk):
             "difficulty": diff,
             "wordLength": word_len,
             "guessLength": guess_len,
+            "tutorial": show_tuto,
             "AIMode": use_ai
         }
         with open("Database/config.json", "w") as config_file:
             json.dump(game_settings, config_file, indent=4)
         messagebox.showinfo('Launching Curdle', "The game will begin momentarily")
-        self.close()
 
     def video(self):
         """Method to play tutorial video"""
         pass
-
-
-    @staticmethod
-    def close():
-        """Static Method: Close PGM"""
-        sys.exit()  # close entire program
-
 
     def get_bg(self):
         """Accessor Method: Panel Background"""
--- setup.bat
+++ setup.bat
@@ -3,6 +3,9 @@ echo Installing depencies, please wait.
 REM Update pip
 python -m pip install --upgrade pip
 
+REM Install CustomTkinter
+python -m pip install CustomTkinter
+
 REM Starting Curdle Game
 
 python3 Jetha_Rehan_Curdle.py
